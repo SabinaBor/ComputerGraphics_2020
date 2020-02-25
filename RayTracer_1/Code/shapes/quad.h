0586935b0ecaebcc972b3cf3cd4d206cf5df53ce
SabinaBor <sboranbayeva@gmail.com>
2020-02-25 04:43:42 +0100

    finished quad

--- RayTracer_1/Code/shapes/quad.h
+++ RayTracer_1/Code/shapes/quad.h
@@ -2,16 +2,18 @@
 #define QUAD_H_
 
 #include "../object.h"
+#include "triangle.h"
 
-class Quad: public Object
-{
-    public:
-        Quad(Point const &v0,
-             Point const &v1,
-             Point const &v2,
-             Point const &v3);
+class Quad : public Object {
+public:
+    Quad(Point const &v0,
+         Point const &v1,
+         Point const &v2,
+         Point const &v3);
 
-        virtual Hit intersect(Ray const &ray);
+    Triangle *tri1, *tri2;
+
+    virtual Hit intersect(Ray const &ray);
 };
 
 #endif
